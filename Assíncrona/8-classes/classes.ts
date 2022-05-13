5b92942a9c46d51a1ec29fa32f533e2f06494e1a
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-13 16:56:19 -0300

    classes

--- Assíncrona/8-classes/classes.ts
+++ Assíncrona/8-classes/classes.ts
@@ -1,12 +1,17 @@
 
 class Data {
-   dia: number;
-   mes: number;
-   ano: number;
+   public dia: number; //por default ele já como uma chave pública!
+   mes: number; //mas posso deixar explícito!
+   ano: number; //aqui eu indico que ano é uma chave privada!
 
-   constructor(dia:number, mes:number, ano:number){
+   constructor(dia:number, mes:number, ano:number = 1994){ //em ano, isso aqui vai ajudar a colocar um valor default caso nem um valor seja passada como parâmetro! 
       this.dia = dia;
       this.mes = mes;
       this.ano = ano;
    }
 }
+
+const data = new Data(1, 1, 2020)
+console.log(data.dia);
+
+const data2 = new Data(1,1)
---
+++ README.md
@@ -0,0 +1,1 @@
+# Aulas e estudo sobre TypeScript
