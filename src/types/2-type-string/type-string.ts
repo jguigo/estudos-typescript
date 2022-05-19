70d230d22a5f18e3ccee7bc4f7c58b066ad7f602
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-19 13:42:43 -0300

    type-string

---
+++ src/types/2-type-string/type-string.ts
@@ -0,0 +1,8 @@
+let tecnologia: string; // depois do nome da váriável passamos o ":string" para sinalizar que é do tipo string
+
+tecnologia = 'javascript';
+tecnologia = 'typescript';
+
+console.log(tecnologia);
+
+// tecnologia = 123; se eu tentar atribuir algum tipo diferente de string, o typescript não vai permitir!
