0e0b761432ac480aa2c389015cbb5b0765e16fb7
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-19 13:49:35 -0300

    type-numbers

---
+++ src/types/3-type-number/type-number.ts
@@ -0,0 +1,7 @@
+let valor1:number; //":number" -> sinaliza que é uma variável do tipo number
+
+//o number pode ser int, float, hex, binary!
+valor1 = 1;
+valor1 = 2.2;
+
+// valor1 = '1'; acusa error porque '1' é uma string!
