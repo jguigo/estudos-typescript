6251553a749d9e7815f0b5df4c89a27722f0958d
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-19 14:45:17 -0300

    type-boolean

---
+++ src/types/6-type-boolean/type-boolean.ts
@@ -0,0 +1,14 @@
+//boolean -> true/false
+
+let estaAutenticado: boolean = true;
+
+estaAutenticado = false;
+
+console.log(estaAutenticado);
+console.log(typeof estaAutenticado);
+
+let codeStatus: number = 1;
+
+estaAutenticado = Boolean(codeStatus);
+console.log(estaAutenticado);
+console.log(typeof estaAutenticado);
