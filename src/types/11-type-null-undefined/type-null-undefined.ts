c48792adbd2fc8a5c5ee593ef1b63adb1023a6db
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-20 23:30:22 -0300

    type-null-undefined

---
+++ src/types/11-type-null-undefined/type-null-undefined.ts
@@ -0,0 +1,10 @@
+// type null é a ausência de valor
+let nome: null; //uma vez atribuida não pode ser alterada,
+
+let nome2: string | null; //normalmente vai ser utilizada com o union type!
+nome2 = null;
+
+//ja o undefined -> não foi atribuido algum valor ou tipo!
+let nomeUser;
+console.log(nomeUser);
+
